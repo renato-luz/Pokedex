7eec972e08ec9cb4a0065ab093532d7ade1c7bbb
Renato Luz <renato_luz21@hotmail.com>
2023-08-01 21:36:45 +0100

    Enable immediate search upon selecting suggestion from the datalist

--- script.js
+++ script.js
@@ -32,25 +32,25 @@ function abrirFechar() {
 const imageScreen = document.querySelector('.main-screen'); // Seleciona o elemento com a classe 'main-screen'
 
 const getPokemonData = (pokemon) => {
-    console.log(pokemon);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        .then((response) => response.json())
-        .then((data) => {
-            let id = ('00' + data.id).slice(-3);
-            imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
-            //nameScreen.innerHTML = data.name;
-            //typeScreen.innerHTML = data.types[0].type.name;
-            //idScreen.innerHTML = `#${data.id}`;
+  console.log(pokemon);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => response.json())
+      .then((data) => {
+          const id = String(data.id).padStart(3, '0'); // Formata o ID com três dígitos preenchidos com zeros à esquerda
+          imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
+          //nameScreen.innerHTML = data.name;
+          //typeScreen.innerHTML = data.types[0].type.name;
+          //idScreen.innerHTML = `#${data.id}`;
 
-            inputField.value = '';
-        })
-        .catch((error) => {
-            console.error('Erro na requisição:', error);
-        });
+          inputField.value = '';
+      })
+      .catch((error) => {
+          console.error('Erro na requisição:', error);
+      });
 };
 
+
 const inputField = document.getElementById('txtBusca'); 
-const searchBtn = document.querySelector('.botao'); 
 
 inputField.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
@@ -58,8 +58,46 @@ inputField.addEventListener('keydown', (event) => {
     }
 });
 
-searchBtn.addEventListener('click', () => {
-    getPokemonData(inputField.value);
-});
 
 /////////////////////////////////////////////////////////////////////
+
+const userInput = document.getElementById("txtBusca");
+const suggestionList = document.getElementById("suggestions");
+
+let jsonOptions = [];
+
+// Fetch JSON data from data.json file
+fetch("pokemonData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    jsonOptions = data;
+  })
+  .catch((error) => console.error("Error fetching JSON data:", error));
+
+userInput.addEventListener("input", handleInput);
+
+function handleInput() {
+  const inputText = userInput.value.trim().toLowerCase();
+
+  // Filtra a lista de pokemons baseado no texto digitado pelo usuário.
+  const filteredOptions = jsonOptions.filter((pokemonName) =>
+    pokemonName.toLowerCase().startsWith(inputText)
+  );
+
+  // Limpa a lista de sugestões existente.
+  suggestionList.innerHTML = "";
+
+  // Adiciona as opções filtradas como sugestões para o campo de entrada.
+  filteredOptions.forEach((pokemonName) => {
+    const option = document.createElement("option");
+    option.value = pokemonName;
+    suggestionList.appendChild(option);
+  });
+}
+
+// Adiciona um ouvinte de evento 'input' ao campo de entrada para acionar a pesquisa quando o valor for alterado.
+userInput.addEventListener("input", () => {
+  getPokemonData(userInput.value);
+});
+
+
